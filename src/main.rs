e8e2d1cf9c4511a22c878b02b3ae9c787e4ac769
LCmaster <luizcv89@gmail.com>
2022-12-02 18:37:12 -0300

    A bit of code cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
 use std::env;
-use std::fs;
-use std::str::FromStr;
-
-use std::collections::HashMap;
-
-use futures::join;
-
-use serde::{Deserialize, Serialize};
-use web3::contract::{Contract, Options, Result};
-use web3::transports::WebSocket;
-use web3::types::{Address, U256};
 
 pub mod bot;
 use crate::bot::Bot;
@@ -29,61 +18,4 @@ async fn main() -> web3::Result<()> {
     let bot = Bot::new(web3, "./config.json", "./abi");
 
     bot.start().await
-
-    // let addresses_file = fs::read_to_string("./addresses.json").expect("Couldn't read the file");
-    // let addresses: Addresses = serde_json::from_str(&addresses_file).unwrap();
-
-    // let eth_address: Address =
-    //     Address::from_str("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee").unwrap();
-    // const EMPTY_HINT: &str = "0x";
-    // const PLATFORM_FEE: u32 = 25;
-    // const AMOUNT_ETH: u32 = 100;
-    // const RECENT_ETH_PRICE: u32 = 1277;
-    // let amount_eth_wei: U256 = U256::exp10(18) * U256::from(AMOUNT_ETH);
-    // let amount_dai_wei: U256 = U256::exp10(18) * U256::from(RECENT_ETH_PRICE);
-
-    // let mut token_map: HashMap<String, Address> = HashMap::new();
-
-    // for token in addresses.token {
-    //     token_map.insert(token.name.to_owned(), token.address.parse().unwrap());
-    // }
-
-    // let dai_abi = "./abi/dai.json";
-    // let dai_addr = token_map["dai"];
-    // let dai_abi_bytes_vector = fs::read(dai_abi).unwrap();
-
-    // let dai_token = Contract::from_json(web3s.eth(), dai_addr, &dai_abi_bytes_vector).unwrap();
-
-    // for dex in addresses.dex {
-    // let dex_abi_filepath = format!("./abi/{}.json", &dex.name);
-    // let dex_abi_bytes_vector = fs::read(dex_abi_filepath).unwrap();
-    // let dex_addr = dex.address.parse().unwrap();
-    // let dex_contract =
-    //     Contract::from_json(web3s.eth(), dex_addr, &dex_abi_bytes_vector).unwrap();
-
-    // let tx1 = get_price_rate(
-    //     &dex_contract,
-    //     dai_token.address(),
-    //     eth_address.to_owned(),
-    //     amount_dai_wei.to_owned(),
-    // );
-    // let tx2 = get_price_rate(
-    //     &dex_contract,
-    //     eth_address.to_owned(),
-    //     dai_token.address(),
-    //     amount_eth_wei.to_owned(),
-    // );
-
-    // if let (Ok((dai_expected_rate, _)), Ok((eth_expected_rate, _))) = join!(tx1, tx2) {
-    //     let kyber_rate_buy = 1.0 / wei_to_eth(dai_expected_rate);
-    //     let kyber_rate_sell = wei_to_eth(eth_expected_rate);
-
-    //     println!("Kyber ETH/DAI");
-    //     println!("Buy: {}; Sell: {}", kyber_rate_buy, kyber_rate_sell);
-    // }
-    // }
-
-    // kyber::KyberDex::new
-
-    // Ok(())
 }
